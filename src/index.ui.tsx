7f4af817254ccc637e89f12576f5e616e7eb648f
Rick <rkessler@microsoft.com>
2024-05-20 21:06:01 -0700

    parking.

--- src/index.ui.tsx
+++ src/index.ui.tsx
@@ -3,28 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { RouterProvider } from 'react-router-dom';
 import { createExtensionClient, InitParams } from '@trident/extension-client';
-import { environments } from './environments';
 import { router } from './apps/shared/routing/router';
 
 export function initialize(params: InitParams) {
     const client = createExtensionClient();
-    const currentEnvironment = environments[params.environmentName] || environments.DEFAULT;
-    // client.onNavigate(route => history.push(route.targetUrl));
-    // const [location, setLocation] = React.useState<string>('');
     client.navigation.onNavigate(route => {
-        router.set
-    //     console.log('here we are');
-    //     history.replace(route.targetUrl);
-    //     setLocation(location);
-
+        router.navigate(route.targetUrl);
     });
-    // client.navigation.navigate()
 
     ReactDOM.render(
         <React.StrictMode>
              <RouterProvider router={router} />
-
-
         </React.StrictMode>,
         document.querySelector('#root')
     );
