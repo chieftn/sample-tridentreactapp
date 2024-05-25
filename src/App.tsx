68e096823f2f81c95b6b5d21e709f90cc7fbaa62
Rick <rkessler@microsoft.com>
2024-05-24 17:40:17 -0700

    parking.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { History } from "history";
 import { ExtensionClientAPI } from "@trident/extension-client";
 import { EnvironmentSetting } from "./environments";
 import { useTranslation } from "react-i18next";
+import type { Common } from "i18next";
 
 interface AppProps {
     history: History;
@@ -11,11 +12,24 @@ interface AppProps {
     environmentConfig: EnvironmentSetting;
 }
 export function App({ history, extensionClient, environmentConfig }: AppProps) {
-    const { t } = useTranslation(['common', 'configure']);
+    const { t, i18n } = useTranslation(['common', 'configure']);
+    console.log(i18n.language);
+
     return (
         <>
-            <span>{t('common:common.configure')}</span>
-            <span>{t('configure:configure')}</span>
+            <div>{t('common:common.yes')}</div>
+            <div>{t('configure:configure')}</div>
+            <div>{t('configure:configure')}</div>
+            <div>{t('configure:plurals.me_one')}</div>
+            <div>{t('configure:plurals.me_other', { count: 12})}</div>
+            <div>{t('configure:plurals.me_underscore_one')}</div>
+            <div>{t('configure:plurals.me_underscore_other', { count: 1})}</div>
+            <div>{t('configure:interpolation.me_interpolated', { me: 'we'})}</div>
+            <div>{t(`configure:typeInterpolation.me_${'one'}`)}</div>
         </>
     );
 };
+
+export const me (): I18nKey<Namespace extends keyof Common>=> {
+    return 'common'
+}
--- src/i18next.d.ts
+++ src/i18next.d.ts
@@ -1,13 +1,15 @@
-import common from './localization/en/common.json';
-import configure from './localization/en/configure.json';
-import explore from './localization/explore.json';
-import insights from './localization/insights.json';
+import common from './localization/locales/en/common.json';
+import configure from './localization/locales/en/configure.json';
+import explore from './localization/locales/en/explore.json';
+import insights from './localization/locales/en/insights.json';
+
 
 declare module 'i18next' {
+  type Common = typeof common;
   interface CustomTypeOptions {
     defaultNS: 'common';
     resources: {
-      common: typeof common;
+      common: Common;
       configure: typeof configure;
       explore: typeof explore;
       insights: typeof insights
